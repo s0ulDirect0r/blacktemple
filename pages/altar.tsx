abbed0bbab089523c961334f8f89502ec1b3be90
s0ulDirect0r <matthewhuff89@gmail.com>
2023-04-21 04:48:07 +0000

    formatting youtube embed attributes

--- pages/altar.tsx
+++ pages/altar.tsx
@@ -9,7 +9,7 @@ const Altar: NextPage = () => {
       <main className={styles.main}>
         <Image alt="Altar Name" src="/freedom-banner.png" width={1500} height={500} />
         {/* Embed a Youtube Link Here */}
-        <iframe width="560" height="315" src="https://www.youtube.com/embed/iuIg7RQAEUM" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+        <iframe width="560" height="315" src="https://www.youtube.com/embed/iuIg7RQAEUM" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen></iframe>
         <br />
         <Link href="/sanctuary"><Image alt="Return Button" src="/return-button.png" width={100} height={50} /></Link>
       </main>
