1da6b36eacd030a7943a4048a9aa5e06ee53d3db
s0ulDirect0r <matthewhuff89@gmail.com>
2023-04-24 18:35:38 +0000

    images and formatting for vestibule

--- pages/vestibule.tsx
+++ pages/vestibule.tsx
@@ -1,21 +1,23 @@
 import type { NextPage } from 'next'
 import Link from 'next/link'
+import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
 const Vestibule: NextPage = () => {
   return (
     <div className={styles.container}>
       <main className={styles.main}>
-        <h2>The Vestibule</h2>
-        <p>Welcome to the Black Temple. This is a place of worship. This is a place to connect with the Divine within the Wilds of the Internet.</p>
-        <p>This is the Vestibule. A place to gather yourself. Get centered. Put yourself in a place to commune with the temple.</p>
-        <p>Take a deep breath. Relax. Turn on some chill music. You&apos;re ok.</p>
-        <p>Please enjoy your time at the Temple.</p>
-
+        <Image src="/vestibule-title.png" alt="Vestibule Title" width={500} height={150} />
+        <div className={styles.vestibule}>
+          <p>Welcome to the Black Temple. This is a place of worship. This is a place to connect with the Divine within the Wilds of the Internet.</p>
+          <p>This is the Vestibule. A place to gather yourself. Get centered. Put yourself in a place to commune with the temple.</p>
+          <p>Take a deep breath. Relax. Turn on some chill music. You&apos;re ok.</p>
+          <p>Please enjoy your time at the Temple.</p>
+        </div>
         <br />
-        <Link href="/sanctuary">Enter The Sanctuary</Link>
+        <Link href="/sanctuary"><Image alt="Sanctuary Button" src="/sanctuary-button.png" width={300} height={150} /></Link>
         <br />
-        <Link href="/">Return to the Portal</Link>
+        <Link href="/"><Image alt="Return Button" src="/return-button.png" width={100} height={50} /></Link>
       </main>
     </div>
   )
