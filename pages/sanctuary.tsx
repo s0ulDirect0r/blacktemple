4e66e61c119038390f19c5c8ca4cdf8e636b2f1c
s0ulDirect0r <matthewhuff89@gmail.com>
2023-04-25 00:12:10 +0000

    add text images to sanctuary

--- pages/sanctuary.tsx
+++ pages/sanctuary.tsx
@@ -1,4 +1,5 @@
 import type { NextPage } from 'next'
+import Image from 'next/image'
 import Link from 'next/link'
 import styles from '../styles/Home.module.css' 
 
@@ -6,13 +7,15 @@ const Sanctuary: NextPage = () => {
   return (
     <div className={styles.container}>
       <main className={styles.main}>
-        <h2>The Sanctuary</h2>
-        <Link href="/altar">Enter The Altar of FREEDOM</Link>
-        {/* <h4>Altar of the HOME</h4>
-        <iframe width="560" height="315" src="https://www.youtube.com/embed/qnJKFwXKtQc" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen></iframe> */}
+        <Image src="/sanctuary-title.png" alt="Sanctuary Title" width={500} height={150} />
+        <div className={styles.vestibule}>
+            <Link href="/altar">Enter The Altar of FREEDOM</Link>
+            {/* <h4>Altar of the HOME</h4>
+            <iframe width="560" height="315" src="https://www.youtube.com/embed/qnJKFwXKtQc" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen></iframe> */}
+        </div>
+
         <br />
-        <Link href="/vestibule">Return to the Vestibule</Link>
-        <Link href="/vestry">Enter the Vestry</Link>
+        <Link href="/vestibule"><Image src="/vestibule-button.png" alt="Vestibule Button" width={100} height={50} /></Link>
       </main>
     </div>
   )
